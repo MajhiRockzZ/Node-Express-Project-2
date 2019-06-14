f471cf93c4cdb189ab47a6cea1b9c0efa2837542
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-06-14 16:17:00 +0530

    feat: creating our google strategy

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: 'mongodb://Sumesh:Sumesh1*@ds237337.mlab.com:37337/storybooks-dev',
+  googleClientID:
+    '773160341091-4vu3jhn6e9cbcn2786f1c83pivs45kk9.apps.googleusercontent.com',
+  googleClientSecret: 'nls5DCTkVtsgcfdDs1Lq_8Vc'
+};
