ba999514cb81908c27442861430a0d85e47f5642
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-01 11:20:32 +0530

    feat: show template format dates

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const stories = require('./routes/stories');
 const keys = require('./config/keys');
 
 // Handlebars Helpers
-const { truncate, stripTags } = require('./helpers/hbs');
+const { truncate, stripTags, formatDate } = require('./helpers/hbs');
 
 // Map global promises
 mongoose.Promise = global.Promise;
@@ -49,7 +49,8 @@ app.engine(
   exphbs({
     helpers: {
       truncate: truncate,
-      stripTags: stripTags
+      stripTags: stripTags,
+      formatDate: formatDate
     },
     defaultLayout: 'main'
   })
